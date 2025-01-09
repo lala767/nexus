b0055added5332ca061436a674a90cc5b2a7d82e
lala767 <lalasadhayo@gmail.com>
2025-01-09 15:08:22 +0500

    Add files via upload

---
+++ json.js
@@ -0,0 +1,183 @@
+var home = 
+[
+    {
+        Num : "1",
+        Name : "goldfish",
+        img : "goldfish.jpg",
+        Information : "The Goldfish is a freshwater fish in the family Cyprinidae of order Cypriniformes. It is commonly kept as a pet in indoor aquariums, and is one of the most popular aquarium fish. Goldfish released into the wild have become an invasive pest in parts of North America."
+    },
+    {
+        Num : "2",
+        Name : "red lionfish",
+        img : "red_lionfish.jpg",
+        Information : "Economic Importance: Although lionfish have been used as a food source in their native range, economically, they are far more important in the aquarium trade. Lionfish are very popular and common aquarium fish, especially in the U.S."
+    },
+    {
+        Num : "3",
+        Name : "wahle shark",
+        img : "wahle-shark.jpg",
+        Information : "There are about 200,000 to 240,000 whale sharks left in all the waters across the globe. This number is small when you consider the sharp decline in their population. Surprisingly, in the last 75 years, the seas have lost more than half their whale shark population."
+    },
+    {
+        Num : "4",
+        Name : "sea turtle",
+        img : "sea-turtle.jpg",
+        Information : ""
+    },
+    {
+        Num : "5",
+        Name : "White and Black Killer Whale",
+        img : "killer-whale.jpg",
+        Information : "Orcas are extremely fast swimmers and have been recorded at speeds of up to 33.5 mph (54 kph). A wild orca pod can cover over 99 miles (160 kilometers) a day, foraging and socializing. They were give the name killer whale by ancient sailors who saw them preying on large whales."
+    },
+    {
+        Num : "6",
+        Name : "Axolotls",
+        img : "axolotls.jpg",
+        Information : "This aquatic character may have a slightly strange appearance and a curious smile, but its personality and engagement with owners makes it rather lovable. Easy to care for, simple to feed, entertaining to watch and boasting a considerable life span, axolotls can provide you with reams of pet fun."
+    },
+    {
+        Num : "7",
+        Name : "jellyfish",
+        img : "jellyfish.jpg",
+        Information : "Many types of jellyfish are fairly harmless to humans. Others can cause severe pain and a full-body (systemic) reaction. These jellyfish cause more-serious problems in people: Box jellyfish. Box jellyfish can cause intense pain and, rarely, life-threatening reactions."
+    },
+    {
+        Num : "8",
+        Name : "starfish",
+        img : "starfish.jpg",
+        Information : "These echinoderms are members of the class Ophiuroidea and are characterized by their long, spindly arms. Their appearance is a little icky to some, but they really do make a good addition to some types of aquariums. They're great for your cleaning crew and very good at disposing of detritus and leftover fish food."
+    },
+    {
+        Num : "9",
+        Name : "sea horse",
+        img : "sea-horse.jpg",
+        Information : "Even with small pectoral fins that assist in steering, seahorses are sadly known to be so delicate that they can become fatally exhausted when waters get rough during storms. However, while they aren't all that durable, seahorses are quite unique in that they can not only move forward, but also up, down and backward."
+    },
+    {
+        Num : "10",
+        Name : "shrimp",
+        img : "shrimp.jpg",
+        Information : "Freshwater shrimp are great if you are looking for something new and exciting to add to your tropical fish tank. While keeping shrimp is a very different experience than keeping tropical fish, they are great for keeping your tank clean from algae and food waste and they are fun to observe."
+    },
+    {
+        Num : "11",
+        Name : "sea anemone",
+        img : "sea-anemone.jpg",
+        Information : " most anemones don't have large enough stinging cells to affect humans, but there are some to be wary of. If you have ever touched a small anemone, the sticky feeling you may have felt is caused by those tiny harpoons as the anemone tries to eat your finger."
+    },
+    {
+        Num : "12",
+        Name : "dolphin",
+        img : "dolphin.jpg",
+        Information : "Dolphins have smooth skin, flippers, and a dorsal fin. They have a long, slender snout with about 100 teeth, and a streamlined body. The single blowhole on top of their head has a flap that opens to reveal a pair of nostrils, which dolphins use for breathing when they surface."
+    },
+    {
+        Num : "13",
+        Name : "crab",
+        img : "crab.jpg",
+        Information : "Sea crabs are crustaceans that don't have a backbone and are protected by a hard shell. Their pincers are used catch food and to fight off predators. The thousands of sea crab species can be found in oceans all around the world. As they grow, these crustaceans molt, or grow new shells for their growing bodies."
+    },
+    {
+        Num : "14",
+        Name : "zebra shark",
+        img : "zebra-sharkjpg",
+        Information : "Juveniles are dark brown with white zebralike stripes, but adults are tan with brown leopardlike spots. Its appearance has given this shark has at least three common names — zebra shark, leopard shark and zebra/leopard shark."
+    },
+    {
+        Num : "15",
+        Name : "octopus",
+        img : "octopus.jpg",
+        Information : "Octopuses. The octopus is a marine mollusk and a member of the class Cephalopoda, more commonly called cephalopods. Cephalopoda means “head foot” in Greek, and in this class of organisms, the head and feet are merged."
+    },
+    {
+        Num : "16",
+        Name : "electric eel",
+        img : "electric-eel.jpg",
+        Information : "Using a combination of its three electric organs, electric eels can generate powerful or weak electrical discharges. Powerful discharges come from the Hunter's and Main organ and are used to defend against predators or stun potential prey."
+    },
+    {
+        Num : "17",
+        Name : "pufferfish",
+        img : "pufferfish.jpg",
+        Information : "The pufferfish's skin is covered in spines and spikes, which are exceedingly hazardous. Carry a toxin called tetrodotoxin (TTX), which is deadly to other fish and humans. You should avoid touching a pufferfish, especially if it is puffed out. Your hands could be damaged and you could die"
+    },
+    {
+        Num : "18",
+        Name : "lobster",
+        img : "lobster.jpg",
+        Information : "Lobster is a type of crustacean rich in protein, omega-3 fatty acids, vitamins, and minerals. Adding it to your diet may be beneficial for weight loss and mental health and may help reduce the risk of heart disease and cancer. However, lobsters are highly allergenic."
+    },
+    {
+        Num : "19",
+        Name : "stingray",
+        img : "stingray.jpg",
+        Information : "Stingrays are dangerous because they can sting you. The stingers on their tails are venomous and harmful to humans. Yet, it's also important to note that while stringrays can be harmful, there are generally only one or two fatal attacks reported each year."
+    },
+    {
+        Num : "20",
+        Name : "Giant Triton",
+        img : "giant-triton.jpg",
+        Information : "The Giant Triton has a humongous spindle-shaped shell that’s size makes it one of the gigantic marine creatures in the world. Its calcium carbonate structure protects the snail’s colorful and soft body, providing it with a safe home."
+    },
+    {
+        Num : "21",
+        Name : "butterflyfish",
+        img : "butterflyfish.jpg",
+        Information : " Butterflyfish are named that way for their striated patterns of black, white, blue, red, orange, and yellow spots and bright butterfly-like colors. They range from silvery to bright white, with yellow tints to the fins and dark spots or bands across the flanks."
+    },
+    {
+        Num : "22",
+        Name : "cat fish",
+        img : "cat-fish.jpg",
+        Information : "Catfish is low in calories and packed with lean protein, healthy fats, vitamins, and minerals. It's particularly rich in heart-healthy omega-3 fats and vitamin B12. It can be a healthy addition to any meal, though deep frying adds far more calories and fat than dry heat cooking methods like baking or broiling."
+    },
+    {
+        Num : "23",
+        Name : "piranhas",
+        img : "piranhas.jpg",
+        Information : "Piranhas have an aggressive image as flesh-eaters, able to dilacerate a human body in seconds. While piranhas do occasionally attack humans, this is only usually when water levels are low and there has never been an official recorded death due to piranha attack."
+    },
+    {
+        Num : "24",
+        Name : "lionfish",
+        img : "lionfish.jpg",
+        Information : " Lionfish can be dangerous to humans due to their potent venom, but incidents of people being stung are infrequent and rarely fatal. A lionfish sting is, however, likely to have some fairly unpleasant effects, from extreme pain and nausea, to convulsions, dizziness, fever and numbness."
+    },
+    {
+        Num : "25",
+        Name : "clown fish",
+        img : "clown-fish.jpg",
+        Information : "Clownfish are famous yet shy omnivores that feed on shrimp, other fish, and various planktons. Their symbiotic relationship with sea anemones is remarkable and helps keep them safe and well-fed."
+    },
+    {
+        Num : "26",
+        Name : "blue tang",
+        img : "blue-tang.jpg",
+        Information : "Blue tangs may look relatively harmless, but when in danger they can raise a pair of razor-sharp, venomous spines on either side of their tails. The fish then whip their bodies from side to side, threatening to stab predators with their toxin-tipped stingers."
+    },
+    {
+        Num : "27",
+        Name : "Alligator gar",
+        img : "alligator-gar.jpg",
+        Information : "World record for an alligator gar goes to a 327-pound fish, estimated to be 95 years old. A Texas fisherman just set a new record for Lake Corpus Christi after catching a 207-pound alligator gar, according to the Texas Parks and Wildlife Department."
+    },
+    {
+        Num : "28",
+        Name : "mandarinfish",
+        img : "mandarinfish.jpg",
+        Information : "The mandarinfish has a few less pleasing qualities. It has spines that will stick the hand of anyone who tries to touch it, and it produces toxins that can be dangerous — especially if they come in contact with open wounds. But these are minor drawbacks."
+    },
+    {
+        Num : "29",
+        Name : "Flower Horn",
+        img : "flower-horn.jpg",
+        Information : "Flowerhorn fish are sometimes considered to be signs of fortune, luck, health, and prosperity by collectors. Flowerhorn fish will change colors throughout their lives."
+    },
+    {
+        Num : "30",
+        Name : "Emperor angelfish",
+        img : "emperor-angelfish.jpg",
+        Information : "Occasionally, a male with one or two females or a juvenile will be spotted together, but rarely with another mature adult male. Emperor angelfish are aggressive and territorial on the reefs, especially towards smaller fish like clownfish. In captivity, emperors are more peaceful."
+    },
+]
